e9e9195bbeab40c6c03bfa25ce512501ed0a0ab4
yungjhin <63853949+yungjhin@users.noreply.github.com>
2022-06-11 22:51:12 +0100

    finished recent posts layout

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { useTheme } from './contexts/ThemeContext';
+import Image from 'next/image';
 
 export default function PostCard({ post }) {
 
@@ -8,13 +9,14 @@ export default function PostCard({ post }) {
 
     return (
         <div className={themeClass + "flex col post-card"}>
-            <div className='img'>
+       
+            <Image layout='responsive' height="100%" width="100%" src="/asdd"/>
 
-            </div>
             <div className='text'>
                 <h2> { post.title } </h2>
                 <p> { post.description } </p>
             </div>
+
             <button onClick={() => router.push(`/posts/${post.slug}`)}>
                 Read more
             </button>
