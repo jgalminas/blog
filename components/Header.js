94011206409ca3776df61c6a9f6920b44c37d0aa
yungjhin <63853949+yungjhin@users.noreply.github.com>
2022-06-18 13:16:52 +0100

    full working theme

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,22 @@
 import Navigation from './Navigation.js';
 import ToggleInput from './ToggleInput.js';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
 
     const { theme , setTheme } = useTheme();
+    const [current, setCurrent] = useState(theme == 'dark' ? true : false);
 
-
-
+    useEffect(() => {
+        setTheme(current ? 'dark' : 'light');
+    }, [current])
 
     return (
         <div className="header flex row">
             <Navigation/>
             <div className='flex-right'>
-                {/* <ToggleInput value={current} onChange={changeTheme}/> */}
-                <button onClick={() => setTheme('dark')}>D</button>
-                <button onClick={() => setTheme('light')}>L</button>
+                <ToggleInput value={current} onChange={() => setCurrent(!current)}/>
             </div>
         </div>
     )
